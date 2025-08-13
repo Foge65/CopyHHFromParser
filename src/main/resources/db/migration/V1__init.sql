2c42f50e50a9709cd1b254b6898cbb1f0d55cbeb
Foge65 <mr.texnik666@gmail.com>
2025-08-13 10:55:18 +0300

    flyway

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE file (
+  uuid uuid NOT NULL PRIMARY KEY,
+  file_path varchar,
+  is_uploaded boolean
+);
--- src/main/resources/init.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS file;
-CREATE TABLE file
-(
-    uuid uuid NOT NULL PRIMARY KEY,
-    file_path   varchar,
-    is_uploaded boolean
-);
